013fee711b2b0ae11bed75fbcc8ea3df70b5336f
ChaitiDutta <simul.dutta.33@gmail.com>
2026-04-09 23:34:31 +0530

    largest cconsequtive sequence in an array

---
+++ merge_Intervals.java
@@ -0,0 +1,8 @@
+// Merge Intervals Problem: 
+// Given an array of intervals intervals where intervals[i] = [start, end], 
+// merge all overlapping intervals. 
+// Example: Input: [[1,3],[2,6],[8,10],[15,18]] 
+// Output: [[1,6],[8,10],[15,18]]
+public class merge_Intervals {
+    
+}
