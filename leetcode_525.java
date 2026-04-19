d1307c074bd484dfb195eabcc9eda0d9f311f6a4
ChaitiDutta <simul.dutta.33@gmail.com>
2026-04-20 00:03:08 +0530

    leetcode 525 problem started

---
+++ leetcode_525.java
@@ -0,0 +1,14 @@
+// Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+// Input: nums = [0,1,1,1,1,1,0,0,0]
+// Output: 6
+// Explanation: [1,1,1,0,0,0] is the longest contiguous subarray with equal number of 0 and 1.
+public class leetcode_525 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,1,1,1,0,0,0};
+        int count = 0;
+        for(int i = 0; i< arr.length; i++){
+            
+        }
+    }
+}
