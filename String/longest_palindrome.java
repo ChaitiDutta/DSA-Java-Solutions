d870ee38fff84802f1ae8c90f57eb82d0fcd6c29
ChaitiDutta <simul.dutta.33@gmail.com>
2026-04-22 22:51:04 +0530

    Leetcode problem 5 started

---
+++ String/longest_palindrome.java
@@ -0,0 +1,13 @@
+// Longest Palindromic Substring
+// Given a string s, return the longest palindromic substring in s.
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+package String;
+
+public class longest_palindrome {
+    public static void main(String[] args) {
+        String s= "cbbd";
+        int left = 0; int right = s.length()-1;
+    }
+}
